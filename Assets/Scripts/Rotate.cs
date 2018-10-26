df5cdcd4476963b06f46680e86dcc19b46873eb4
Rll19 <katana20@hotmail.com>
2018-10-26 17:03:34 -0600

    Base inicial del proyecto

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+Rotate the object
+*/
+public class Rotate : MonoBehaviour {
+
+	public float speed = 50;
+
+	void Update () {
+		
+		transform.Rotate(new Vector3(0,0,Time.deltaTime * speed));
+
+	}
+}
