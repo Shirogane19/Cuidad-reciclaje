e3d80cc8cb1f0b623100ca5c1bf2bdfb7e092dda
DESKTOP-S6ONV06\marco <mdurane@ucenfotec.ac.cr>
2019-01-22 12:04:17 -0600

    Arreglo de bug cuando se obtiene 10 puntos en el juego de separar basura Arreglo del box collider de los prefabs de la basura.

---
+++ Assets/Scripts/drag.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drag : MonoBehaviour {
+
+    float distance = 10;
+    void OnMouseDrag()
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = objPosition;
+    }
+
+
+    //float distance = 10;
+    //void OnTouchDrag()
+    //{
+    //    Vector3 touchPosition = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, distance);
+    //    Vector3 objPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+    //    transform.position = objPosition;
+    //}
+
+    //void OnTouchDrag()
+    //{
+    //    Vector3 touchPosition = new Vector3(Input.touch.position.x, Input.touches.position.y, transform.position.z);
+    //    Vector3 objPosition = Camera.main.ScreenToWorldPoint(touchPosition);
+    //    transform.position = objPosition;
+    //}
+}
