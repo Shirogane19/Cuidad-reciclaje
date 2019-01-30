e06b8bff099074ffa8b62f20c90c806d4b11c596
Rll19 <katana20@hotmail.com>
2019-01-30 16:49:50 -0600

    Version 1.0 release

--- Assets/Scripts/SetVisualElements.cs
+++ Assets/Scripts/SetVisualElements.cs
@@ -83,7 +83,7 @@ public class SetVisualElements : MonoBehaviour {
 	{
 		Tips = new List<string>();
 		Tips.Add ("Reciclar: Consiste en recoger y procesar los desechos para luego obtener productos nuevos.");
-		Tips.Add ("Reducir: Se trata de evitar todas las acciones que generen desperdicios y basura innecesariamente.");
+		Tips.Add ("Reducir: Se trata de evitar todas las acciones que generen desperdicios y residuos innecesariamente.");
 		Tips.Add ("Reutilizar: Busca aprovechar los desechos para crear nuevos productos y darles otros usos.");
 		Tips.Add ("Basureros de color verde, aqui van los desechos orgánicos como restos de alimentos, frutas, verduras, cascaras y residuos de jardin.");
 		Tips.Add ("Basureros de color azul, aqui van los desechos de envases como botellas y bolsas o envases plasticos.");
@@ -96,25 +96,25 @@ public class SetVisualElements : MonoBehaviour {
 		Tips.Add ("Una buena práctica para reducir el gasto de agua, es cerrar la llave al lavarse los dientes, las manos o los platos.");
 		Tips.Add ("Una buena práctica para reducir el gasto de agua, es decirle a otras personas cuando el grifo está goteando.");
 		Tips.Add ("Una buena práctica para reducir el gasto de agua, es tomarse el agua que se sirve en un vaso.");
-		Tips.Add ("Una buena práctica para la conservación de la energía, es apagando la luz cuando no la necesite.");
-		Tips.Add ("Una buena práctica para la conservación de la energía, es utilizar la luz del día en vez de una bombilla.");
-		Tips.Add ("Una buena práctica para la conservación de la energía, es apagando el aire acondicionado cuando no se necesite.");
-		Tips.Add ("Una buena práctica para la conservación de la energía, es apagando el televisor cuando no esté en uso.");
-		Tips.Add ("Una buena práctica para la conservación de la energía, es desconectando todos los enchufes cuando no este usando los aparatos.");
-		Tips.Add ("Una buena práctica para la conservación de la energía, es utilizando bombillo de menos consumo eléctrico como las LED.");
+		Tips.Add ("Una buena práctica para la conservación de la energía, es apagar la luz cuando no la necesite.");
+		Tips.Add ("Una buena práctica para la conservación de la energía, es utilizar la luz del día en vez de un bombillo.");
+		Tips.Add ("Una buena práctica para la conservación de la energía, es apagar el aire acondicionado cuando no se necesite.");
+		Tips.Add ("Una buena práctica para la conservación de la energía, es apagar el televisor cuando no esté en uso.");
+		Tips.Add ("Una buena práctica para la conservación de la energía, es desconectar todos los enchufes cuando no este usando los aparatos.");
+		Tips.Add ("Una buena práctica para la conservación de la energía, es utilizar un bombillo de menos consumo eléctrico como las LED.");
 		Tips.Add ("La contaminación es el conjunto de todas las acciones del ser humano que dañan y enferman a nuestro planeta.");
 		Tips.Add ("Una forma de contaminación es la del aire, ejemplo de esto sería la quema de combustible fósil, consumo de tabaco, productos de limpieza, etc...");
-		Tips.Add ("Una forma de contaminación es la del agua, ejemplo de esto sería la tira de basura, desechos químicos de fábricas, derramamiento de petreolo, etc...");
+		Tips.Add ("Una forma de contaminación es la del agua, ejemplo de esto sería la tira de residuos, desechos químicos de fábricas, derramamiento de petreolo, etc...");
 		Tips.Add ("Una forma de contaminación es la acústica o sonido, ejemplo de esto sería el tráfico de las calles, areas de construcción, los conciertos de rock, etc...");
 		Tips.Add ("Los carros emiten mucho dióxido de carbono (CO2) por lo que se consideran una de las formas más comunes de contaminación.");
 		Tips.Add ("La emisión de gases causa que el planeta se caliente, daña la vegetación, afecta el agua, repercute en los animales y a nosotros.");
-		Tips.Add ("Una forma de transporte saludable es usar el transporte público.");
-		Tips.Add ("Una forma de transporte saludable es montar en bicicleta.");
-		Tips.Add ("Una forma de transporte saludable es utilizar automóviles elétricos.");
-		Tips.Add ("Una forma de transporte saludable es compartir un carro con conocidos que van al mismo lugar.");
+		Tips.Add ("Una forma de transporte saludable, es usar el transporte público.");
+		Tips.Add ("Una forma de transporte saludable, es montar en bicicleta.");
+		Tips.Add ("Una forma de transporte saludable, es utilizar automóviles elétricos.");
+		Tips.Add ("Una forma de transporte saludable, es compartir un carro con conocidos que van al mismo lugar.");
 		Tips.Add ("Las plantas ayudan a disminuir la cantidad de CO2 en el planeta porque lo absorbe y lo convierte en oxígeno a través de un proceso llamado fotosíntesis.");
 		Tips.Add ("Las plantas son una importante clave de nuestra sobrevivencia porque produce óxigeno.");
-		Tips.Add ("Hay que cuidar nuestros parque nacionales, evitando acciones como tirar basura, alimentar sin permiso a los animales, tocar los animales o las plantas.");
+		Tips.Add ("Hay que cuidar nuestros parque nacionales, evitando acciones como tirar residuos, alimentar sin permiso a los animales, tocar los animales o las plantas.");
 		
 	}
 	//----------------------------------------------------------
